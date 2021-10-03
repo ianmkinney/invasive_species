1a76965950f9d13d0e32e95c28ccbe00e89b1895
ianmkinney <ikinney51@gmail.com>
2021-10-03 12:45:12 -0400

    Test 2

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h1>Invasive Species in Georgia</h1>"
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(threaded=True, port=5000)
