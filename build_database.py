8b10c483d369c36fb67f3a19a71b537286375f6f
ianmkinney <ikinney51@gmail.com>
2021-10-03 16:25:41 -0400

    Updated Database

--- build_database.py
+++ build_database.py
@@ -5,12 +5,14 @@ import time
 from sqlalchemy import create_engine
 from connection_string import connection_string
 
+# Paerform API Calls
 observationsSep = get_observations(place_id=23, per_page=200, introduced=True, quality_grade='research', month=9)
 observationsAug = get_observations(place_id=23, per_page=200, introduced=True, quality_grade='research', month=8)
 observationsJul = get_observations(place_id=23, per_page=200, introduced=True, quality_grade='research', month=7)
 observationsJun = get_observations(place_id=23, per_page=200, introduced=True, quality_grade='research', month=6)
 observationsMay = get_observations(place_id=23, per_page=200, introduced=True, quality_grade='research', month=5)
 
+# Create array of dataframs from api jsons
 dfs = []
 
 dfSep = pd.DataFrame(observationsSep['results'])
@@ -24,13 +26,33 @@ dfs.append(dfJun)
 dfMay = pd.DataFrame(observationsMay['results'])
 dfs.append(dfMay)
 
+# Add new fields from dictionaries within data
+for df in dfs:
+    df['iconic_taxon_name'] = ""
+    df['name'] = ""
+    df['preferred_common_name'] = ""
+    df['wikipedia_url'] = ""
+    i = 0
+    for dict in df['taxon']:
+        df.at[i,'iconic_taxon_name'] = dict['iconic_taxon_name']
+        df.at[i,'name'] = dict["name"]
+        if 'preferred_common_name' in dict:
+            df.at[i, 'preferred_common_name'] = dict['preferred_common_name']
+        if 'wikipedia_url' in dict:
+            df.at[i, 'wikipedia_url'] = dict['wikipedia_url']
+
+        i = i+1
+
 print("API calls completed")
 
+# Create simplified dataframes to upload to database
 updated_dfs = []
 for df in dfs:
-    new_df = df[['time_observed_at', 'species_guess', 'place_guess', 'location']]
+    new_df = df[['time_observed_at', 'species_guess', 'place_guess', 'location', 'iconic_taxon_name', 'name', 'preferred_common_name', 'wikipedia_url']]
     updated_dfs.append(new_df)
 
+
+# Upload to database
 db = create_engine(connection_string)
 
 conn = db.connect()
