7169c88ef9f5523cbc03faa29dd11d3a2eab3a44
ianmkinney <ikinney51@gmail.com>
2021-10-04 02:43:56 -0400

    Fixed views

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template, url_for
 app = Flask(__name__, template_folder='templates')
 
 @app.route('/')
